7176e2a20c685f08a98cf2643348bd9912512c2f
Weili Shi <me@shi-weili.com>
2018-07-30 10:03:13 -0400

    Continue train

--- scripts/train_planet_mars.sh
+++ scripts/train_planet_mars.sh
@@ -1,3 +1,3 @@
-python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 4 --dataroot 'datasets/simple_grid' --no_flip --no_instance
+python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 4 --dataroot 'datasets/simple_grid' --no_flip --no_instance --continue_train
 
 sudo shutdown -h now
