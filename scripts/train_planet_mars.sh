81a1292ef3dd7f858cc607b1f56d0978ddcb7947
Weili Shi <me@shi-weili.com>
2018-08-05 17:31:19 -0400

    Remove tensorboard log

--- scripts/train_planet_mars.sh
+++ scripts/train_planet_mars.sh
@@ -2,6 +2,6 @@
 
 # python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance --batchSize 8 --gpu_ids 0,1,2,3,4,5,6,7
 
-python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance --continue_train --batchSize 2 --gpu_ids 0,1 --tf_log
+python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance --continue_train --batchSize 2 --gpu_ids 0,1
 
 sudo shutdown -h now
