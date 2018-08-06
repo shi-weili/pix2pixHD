d2bfb1fd402a002a2a16777183e4938d785e2b22
Weili Shi <me@shi-weili.com>
2018-08-06 12:54:42 -0400

    Don't continue train

--- scripts/train_planet_mars.sh
+++ scripts/train_planet_mars.sh
@@ -2,6 +2,6 @@
 
 # python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance --batchSize 8 --gpu_ids 0,1,2,3,4,5,6,7
 
-python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance --continue_train
+python3 train.py --planet_mars --name planet_mars_gen_2 --loadSize 512 --fineSize 512 --label_nc 0 --input_nc 7 --dataroot 'datasets/seamless_grid' --no_flip --no_instance
 
 sudo shutdown -h now
